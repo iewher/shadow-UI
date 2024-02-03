53536f8eb577d3b8e3b1322c2d47653781cfe889
iewher <goshagang@mail.ru>
2024-02-03 12:42:03 +0500

    feat(design): added header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,10 @@
+import Banner from "@/components/banner";
 import styles from "./page.module.scss";
-
-const Card = ({
-  title,
-  description,
-}: {
-  title: string;
-  description: string;
-}) => {
-  return (
-    <div className={styles.Card}>
-      <h5 className={styles.CardTitle}>{title}</h5>
-      <p className={styles.CardDescription}>{description}</p>
-    </div>
-  );
-};
 
 export default function Home() {
   return (
     <div className={styles.Page}>
-      <div className={styles.Banner}>
-        <h1>Shadow UI V0.1</h1>
-        <p>
-          Help designers/developers building beautiful products more flexible
-          and working with happiness
-        </p>
-        <button>Getting started</button>
-        <div className={styles.Cards}>
-          <Card title={"Shadow UI prereleased"} description={"Скоро будет выпущена первая версия"} />
-          <Card title={"Shadow UI prereleased"} description={"Скоро будет выпущена первая версия"} />
-          <Card title={"Shadow UI prereleased"} description={"Скоро будет выпущена первая версия"} />
-        </div>
-      </div>
+      <Banner />
     </div>
   );
 }
